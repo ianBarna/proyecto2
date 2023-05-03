9efc465ea4359b1ffbcf75f23478c4cd9c915b27
Juan Enterria <ianiusovi@gmail.com>
2023-05-03 21:17:08 +0200

    hecho hasta el miércoles 3 de mayo...

--- tuganado/routes/index.routes.js
+++ tuganado/routes/index.routes.js
@@ -6,31 +6,31 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
-router.get("/", (req, res, next) => {
+router.get("../auth/login", (req, res, next) => {
   res.render("auth/login");
 });
 
-router.get("/", (req, res, next) => {
+router.get("/auth/register", (req, res, next) => {
   res.render("auth/register");
 });
 
-router.get("/", (req, res, next) => {
+router.get("/ganado/add", (req, res, next) => {
   res.render("ganado/add");
 });
 
-router.get("/", (req, res, next) => {
+router.get("/ganado/list", (req, res, next) => {
   res.render("ganado/list");
 });
 
-router.get("/", (req, res, next) => {
+router.get("/ganado/single", (req, res, next) => {
   res.render("ganado/single");
 });
 
-router.get("/", (req, res, next) => {
+router.get("/ganado/add", (req, res, next) => {
   res.render("ganado/add");
 });
 
-router.get("/", (req, res, next) => {
+router.get("/ganado/edit", (req, res, next) => {
   res.render("ganado/edit");
 });
 
