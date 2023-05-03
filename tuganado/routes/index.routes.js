40a259ac9850b9454cdefe5f61fae009ea60c9e7
Juan Enterria <ianiusovi@gmail.com>
2023-05-03 20:40:51 +0200

    primeras rutas

--- tuganado/routes/index.routes.js
+++ tuganado/routes/index.routes.js
@@ -6,4 +6,33 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
+router.get("/", (req, res, next) => {
+  res.render("auth/login");
+});
+
+router.get("/", (req, res, next) => {
+  res.render("auth/register");
+});
+
+router.get("/", (req, res, next) => {
+  res.render("ganado/add");
+});
+
+router.get("/", (req, res, next) => {
+  res.render("ganado/list");
+});
+
+router.get("/", (req, res, next) => {
+  res.render("ganado/single");
+});
+
+router.get("/", (req, res, next) => {
+  res.render("ganado/add");
+});
+
+router.get("/", (req, res, next) => {
+  res.render("ganado/edit");
+});
+
+
 module.exports = router;
